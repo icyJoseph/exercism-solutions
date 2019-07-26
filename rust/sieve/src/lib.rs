be5ca8583a602a21a6184723f3619edf1d619657
Joseph Chamochumbi <sephxd1234@gmail.com>
2019-07-26 23:40:29 +0200

    feat: even faster sieve! done in one pass ⚡

--- rust/sieve/src/lib.rs
+++ rust/sieve/src/lib.rs
@@ -6,13 +6,17 @@ pub fn primes_up_to(upper_bound: u64) -> Vec<u64> {
         .map(|num| if num < 2 { (false, num) } else { (true, num) })
         .collect();
 
+    // the primes to return at the end
+    let mut primes: Vec<u64> = vec![];
+
     // this for loop copies the sieve and loops over it
     // so that we can safely mutate the original sieve
     // i is numerically equal to number, but of type usize!
-    for (i, (is_prime, number)) in sieve.to_vec().iter().enumerate() {
+    for (i, _) in sieve.to_vec().iter().enumerate() {
+        let (is_prime, number) = sieve[i];
         // optimization, run only until the sqrt of upper_bound (length)
-        if number * number <= upper_bound {
-            if *is_prime {
+        if is_prime {
+            if number * number <= upper_bound {
                 // the sieve says, if the current number is prime
                 // then i*(i+n) where
                 // 0<= n <= upper_bound or until i*(i+n) is greater than upper_bound
@@ -20,23 +24,20 @@ pub fn primes_up_to(upper_bound: u64) -> Vec<u64> {
                 // so we iterate over all positive integers from 0
                 // until we have i*(i+n) is greater than upper_bound
                 let steps: Vec<usize> = (0..).take_while(|n| i * (i + n) < sieve.len()).collect();
-
+                // walk the sieve declaring non primes
                 for step in steps {
                     sieve[i * (i + step)].0 = false;
                 }
+
+                primes.push(number);
+                continue;
             }
-            continue;
-        }
-        break;
-    }
-
-    let mut primes: Vec<u64> = vec![];
-
-    for (is_prime, number) in sieve {
-        if is_prime {
+            // since we are mutating the sieve
+            // those which are not prime, can be safely added here
+            // this reduces the problem to ONE pass
             primes.push(number);
         }
     }
 
-    return primes;
+    primes
 }
