d4eb0dbcb74d92b9b61b2cb2dbed12ce70972057
Joseph Chamochumbi <sephxd1234@gmail.com>
2019-07-26 23:02:43 +0200

    feat: optimize sieve! ⚡

--- rust/sieve/src/lib.rs
+++ rust/sieve/src/lib.rs
@@ -1,34 +1,28 @@
 pub fn primes_up_to(upper_bound: u64) -> Vec<u64> {
     // start with a list of length upper_bound, filled with true
-    // we assume all to be primes and loop over to prove it
-    // false means it is not a prime
-    let mut sieve: Vec<bool> = (2..=upper_bound).map(|_| true).collect();
-    let number_list: Vec<u64> = (2..=upper_bound).collect();
-    let length = number_list.len();
-    // upper_bound to usize when optimizing the loop
+    // except for the first two numbers, 0 and 1, the rest
+    // are all assumed to be primes
+    let mut sieve: Vec<(bool, u64)> = (0..=upper_bound)
+        .map(|num| if num < 2 { (false, num) } else { (true, num) })
+        .collect();
 
-    for (i, number) in number_list.iter().enumerate() {
+    // this for loop copies the sieve and loops over it
+    // so that we can safely mutate the original sieve
+    // i is numerically equal to number, but of type usize!
+    for (i, (is_prime, number)) in sieve.to_vec().iter().enumerate() {
         // optimization, run only until the sqrt of upper_bound (length)
         if number * number <= upper_bound {
-            let is_prime = sieve[i];
-            // the index in the sieve + 2 = number
-            // can't use number directly because it is u64 and indexes are usize
-            // u64 <-> usize is unsafe
-            let number_value = i + 2;
-            if is_prime {
-                // so calculate the non primes ahead by doing:
-                //  number_value * (number_value + step)
-                // and then calculate the index in the sive by mapping with:
-                // x => x - 2
-                let non_primes_index_ahead: Vec<usize> = (0..=length)
-                    .map(|step| number_value * (number_value + step))
-                    .map(|x| x - 2)
-                    .collect();
+            if *is_prime {
+                // the sieve says, if the current number is prime
+                // then i*(i+n) where
+                // 0<= n <= upper_bound or until i*(i+n) is greater than upper_bound
+                // are all not primes!
+                // so we iterate over all positive integers from 0
+                // until we have i*(i+n) is greater than upper_bound
+                let steps: Vec<usize> = (0..).take_while(|n| i * (i + n) < sieve.len()).collect();
 
-                for index in non_primes_index_ahead {
-                    if index < length {
-                        sieve[index] = false;
-                    }
+                for step in steps {
+                    sieve[i * (i + step)].0 = false;
                 }
             }
             continue;
@@ -38,9 +32,9 @@ pub fn primes_up_to(upper_bound: u64) -> Vec<u64> {
 
     let mut primes: Vec<u64> = vec![];
 
-    for (i, is_prime) in sieve.iter().enumerate() {
-        if *is_prime {
-            primes.push(number_list[i]);
+    for (is_prime, number) in sieve {
+        if is_prime {
+            primes.push(number);
         }
     }
 
